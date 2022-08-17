ae68609c65f5b8d64021de43f9d5b72e700b757f
William Leung <gwilliamleung@gmail.com>
2022-08-17 19:16:27 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Main from "./components/Main"
+import Body from "./components/Body"
+
+export default function App(){
+    <div className="container">
+        <Main />
+        <Body />
+    </div>
+}
